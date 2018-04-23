31249adfdc5dae3f104390ea554d2fdf66492073
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to: use a custom numeric measure unit in Automatic scale mode
+
+
+This example demonstrates how to specify a custom numeric measure unit in <strong>Automatic</strong> scale mode.
+
+
+<h3>Description</h3>
+
+To do this, assign an object of a class implementing the&nbsp;<a href="https://documentation.devexpress.com/#WPF/clsDevExpressXpfChartsINumericMeasureUnitsCalculatortopic">INumericMeasureUnitsCalculator</a>&nbsp;interface to the&nbsp;<a href="https://documentation.devexpress.com/#WPF/DevExpressXpfChartsAutomaticNumericScaleOptions_AutomaticMeasureUnitsCalculatortopic">AutomaticMeasureUnitsCalculator</a>&nbsp;property of an <a href="https://documentation.devexpress.com/#WPF/clsDevExpressXpfChartsAutomaticNumericScaleOptionstopic">AutomaticNumericScaleOptions</a>&nbsp;object assigned to <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfChartsAxisX2D_NumericScaleOptionstopic">AxisX2D.NumericScaleOptions</a>.
+
+<br/>
+
+
---
+++ VB/CustomNumericMeasureUnitsCalculatorSample/MainWindow.xaml.vb
@@ -0,0 +1,39 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+
+Namespace CustomNumericMeasureUnitsCalculatorSample
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+            DataContext = GenerateData(10000)
+        End Sub
+
+        Private Function GenerateData(ByVal count As Integer) As List(Of DataPoint)
+            Dim points As New List(Of DataPoint)(count)
+            Dim generator As New Random()
+            For i As Integer = 0 To count - 1
+                points.Add(New DataPoint With {.X = i, .Y = generator.NextDouble()})
+            Next i
+            Return points
+        End Function
+    End Class
+
+    Public Class DataPoint
+        Public Property X() As Double
+        Public Property Y() As Double
+    End Class
+End Namespace
